e0c175e001a9775bd7ecaa6b9a38eb70598b7e94
deedeeh <dinahafez.8@gmail.com>
2018-09-19 17:41:10 +0100

    Add OO counting sentences lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split = (self.split(/[.!?]/).reject {|str| str == ""}).count
   end
+  
 end
+
+p 'eof'
