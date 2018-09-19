7a578147084aeabc35ccfea14f009a58a413237c
ssong-eu <sangyoon.song@gmail.com>
2018-09-19 12:58:19 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+    # if self[-1] == "."
+    #   return true
+    # else
+    #   false
+    # end
   end
 
   def question?
-
+    self.end_with?("?")
+    # if self[-1] == "?"
+    #   return true
+    # else
+    #   false
+    # end
   end
 
   def exclamation?
-
+    self.end_with?("!")
+    # if self[-1] == "!"
+    #   return true
+    # else
+    #   false
+    # end
   end
 
   def count_sentences
-
+    num = self.split(/[?!.]/)
+    num.reject!{|sent| sent.empty?}
+    num.count
   end
+
 end
